4996a10c8caee031f1edb6bc02281bdd24b4d717
Kenneth Ferrell <kferrell@alphaworks.tech>
2022-04-29 13:16:47 -0700

    feat(assets): import images and create index files for exporting

---
+++ src/assets/navIcons/index.js
@@ -0,0 +1,7 @@
+import { ReactComponent as AboutMeIcon } from "./me.svg";
+import { ReactComponent as ContactIcon } from "./contact.svg";
+import { ReactComponent as GitHubIcon } from "./github.svg";
+import { ReactComponent as HomeIcon } from "./home.svg";
+import { ReactComponent as ProjectsIcon } from "./projects.svg";
+
+export { AboutMeIcon, ContactIcon, GitHubIcon, HomeIcon, ProjectsIcon };
---
+++ src/assets/skillsIcons/index.js
@@ -0,0 +1,19 @@
+import { ReactComponent as CssIcon } from "./css.svg";
+import { ReactComponent as FigmaIcon } from "./figma.svg";
+import { ReactComponent as HtmlIcon } from "./html.svg";
+import { ReactComponent as JsIcon } from "./js.svg";
+import { ReactComponent as ReactIcon } from "./react.svg";
+import { ReactComponent as ReduxIcon } from "./redux.svg";
+import { ReactComponent as StyledCompIcon } from "./styledcomp.svg";
+import { ReactComponent as TsIcon } from "./ts.svg";
+
+export {
+  CssIcon,
+  FigmaIcon,
+  HtmlIcon,
+  JsIcon,
+  ReactIcon,
+  ReduxIcon,
+  StyledCompIcon,
+  TsIcon,
+};
